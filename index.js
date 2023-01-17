5a3a4b561f35c5400f0e60a55457cf728bfe561d
itsPeetah <pietro.g.moroni@gmail.com>
2023-01-17 10:40:09 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref } from "firebase/database";
-
-const useRTDBRef = (config, path) => {
-  const app = initializeApp(config);
-  const db = getDatabase(app);
-  return ref(db, path);
-};
+import { default as useRTDBRef } from "./useRTDBRef";
 
 export default useRTDBRef;
